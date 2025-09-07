d061e31abdc7f0ec82e1c0adcfbb28dfabf9353b
Devtel0912 <devtel0912@gmail.com>
2025-09-07 17:34:03 -0400

    New Changes

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ from app.main import app
 def testmain():
     client = app.test_client()
     response = client.get('/')
-    assert response.status_code == 220
+    assert response.status_code == 200
     assert response.get_json() == {'message':"Hello World I am Dev Telang!"}
     
     
